zhonyu555/PrusaSlicer

FontItem improvmement: Use default constructors to let the compiler generate move constructor and move assignement. Also used some move operators to reduce unnecessary memory allocations.

src/libslic3r/Format/3mf.cpp
@@ -3374,14 +3374,14 @@ std::optional<TextConfiguration> TextConfigurationSerialization::read(const char
     std::string weight = get_attribute_value_string(attributes, num_attributes, FONT_WEIGHT_ATTR);
     if (!weight.empty()) fp.weight = weight;
 
-    std::string font_name  = "";
+    std::string font_name {};
     std::string font_descriptor = get_attribute_value_string(attributes, num_attributes, FONT_DESCRIPTOR_ATTR);
     std::string type_str = get_attribute_value_string(attributes, num_attributes, FONT_DESCRIPTOR_TYPE_ATTR);
     FontItem::Type type = TextConfigurationSerialization::get_type(type_str);
-    FontItem fi(font_name, font_descriptor, type, fp);
+    FontItem fi{ font_name, std::move(font_descriptor), type, std::move(fp) };
 
     std::string text = get_attribute_value_string(attributes, num_attributes, TEXT_DATA_ATTR);
-    return TextConfiguration(fi, text);
+    return TextConfiguration(std::move(fi), std::move(text));
 }
 
 

MythicReliquary/LibreSlicer

Cherry picked commit: b09bc0c4b5b3045e0fea67289b4f2c718131927b

src/libslic3r/Format/3mf.cpp
@@ -3395,10 +3395,10 @@ std::optional<TextConfiguration> TextConfigurationSerialization::read(const char
     std::string font_descriptor = get_attribute_value_string(attributes, num_attributes, FONT_DESCRIPTOR_ATTR);
     std::string type_str = get_attribute_value_string(attributes, num_attributes, FONT_DESCRIPTOR_TYPE_ATTR);
     FontItem::Type type = TextConfigurationSerialization::get_type(type_str);
-    FontItem fi(style_name, font_descriptor, type, fp);
+    FontItem fi{ style_name, std::move(font_descriptor), type, std::move(fp) };
 
     std::string text = get_attribute_value_string(attributes, num_attributes, TEXT_DATA_ATTR);
-    return TextConfiguration(fi, text);
+    return TextConfiguration(std::move(fi), std::move(text));
 }
 
 