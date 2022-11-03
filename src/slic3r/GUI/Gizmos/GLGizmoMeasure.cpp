MythicReliquary/LibreSlicer

commit 74bcf7aff5b2121b31b66a5defb7c1b92c897e78

Measuring: validation for zero distance moved from backend to frontend

src/slic3r/GUI/Gizmos/GLGizmoMeasure.cpp
@@ -905,7 +905,7 @@ void GLGizmoMeasure::render_dimensioning()
         return;
 
     auto point_point = [this, shader](const Vec3d& v1, const Vec3d& v2, float distance) {
-        if (v1.isApprox(v2))
+        if ((v2 - v2).squaredNorm() < 0.000001)
             return;
 
         const Camera& camera = wxGetApp().plater()->get_camera();

src/libslic3r/Measure.cpp
@@ -1060,14 +1060,6 @@ MeasurementResult get_measurement(const SurfaceFeature& a, const SurfaceFeature&
         else
             result.angle = angle_plane_plane(f1.get_plane(), f2.get_plane());
     }
-
-    // validation
-    if (result.distance_infinite.has_value() && result.distance_infinite->dist < EPSILON)
-        result.distance_infinite.reset();
-    if (result.distance_strict.has_value() && result.distance_strict->dist < EPSILON)
-        result.distance_strict.reset();
-    if (result.angle.has_value() && std::abs(result.angle->angle) < EPSILON)
-        result.angle.reset();
     
     return result;
 }

commit 97c7da59f158a98db609c39b76cd8a25e158914a

Fixed typo introduced with 74bcf7aff5b2121b31b66a5defb7c1b92c897e78

src/slic3r/GUI/Gizmos/GLGizmoMeasure.cpp
@@ -905,7 +905,7 @@ void GLGizmoMeasure::render_dimensioning()
         return;
 
     auto point_point = [this, shader](const Vec3d& v1, const Vec3d& v2, float distance) {
-        if ((v2 - v2).squaredNorm() < 0.000001)
+        if ((v2 - v1).squaredNorm() < 0.000001 || distance < 0.001f)
             return;
 
         const Camera& camera = wxGetApp().plater()->get_camera();